f97030da1c85ac045c2e5927972d17466e5139a6
furlborg <50270335+furlborg@users.noreply.github.com>
2021-09-07 18:24:36 -0500

    Delete vue initial template

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import {store} from './store'
+import {naive} from './plugins/naive'
+import axios from "axios"
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router, axios).use(store).use(naive).mount('#app')
