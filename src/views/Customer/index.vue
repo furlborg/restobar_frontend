3ae30841514d00eb2449370426fd6c27701ca4df
furlborg <50270335+furlborg@users.noreply.github.com>
2021-09-08 15:31:09 -0500

    Customer view initial template

--- src/views/Customer/index.vue
+++ src/views/Customer/index.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>
-    Customer View
-  </div>
+  <n-card title="Clientes" :bordered="false" :segmented="{ content: 'hard' }">
+    <template #header-extra>
+      <n-button type="primary" @click="">
+        <template #icon>
+          <n-icon>
+            <v-icon name="la-user-plus-solid" />
+          </n-icon>
+        </template>
+        Nuevo
+      </n-button>
+    </template>
+    <n-collapse arrow-placement="right">
+    </n-collapse>
+  </n-card>
 </template>
 
 <script>
-import {defineComponent} from "vue";
+import {defineComponent} from "vue"
+import {OhVueIcon} from "@/plugins/icon"
 
 export default defineComponent({
   name: "Customer",
+  components: {
+    'v-icon': OhVueIcon
+  },
   setup() {
-
-    return {
-
-    }
+    return {}
   }
 })
 </script>
