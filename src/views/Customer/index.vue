67063d6213bf8325ecea59e9b2adaa0b8592f997
furlborg <50270335+furlborg@users.noreply.github.com>
2021-09-14 11:43:30 -0500

    Update customer view
    -Fixed customer filters

--- src/views/Customer/index.vue
+++ src/views/Customer/index.vue
@@ -1,68 +1,41 @@
 <template>
   <n-card title="Clientes" :bordered="false" :segmented="{ content: 'hard' }">
+    <template #header-extra>
+      <n-button type="primary" @click="">
+        <template #icon>
+          <n-icon>
+            <v-icon name="la-user-plus-solid" />
+          </n-icon>
+        </template>
+        Nuevo
+      </n-button>
+    </template>
     <n-collapse class="mb-3" arrow-placement="right">
       <n-collapse-item title="Búsqueda" name="1">
-        <template #header-extra>
-          <n-button type="primary" @click="">
-            <template #icon>
-              <n-icon>
-                <v-icon name="la-user-plus-solid" />
-              </n-icon>
-            </template>
-            Nuevo
-          </n-button>
-        </template>
         <!-- Search Customer Form -->
         <n-form>
-          <n-grid cols="1 s:1 m:3 l:3 xl:5 2xl:5" :x-gap="12" responsive="screen">
-            <n-grid-item>
-              <n-grid x-gap="12" :cols="2" >
-                <n-grid-item>
-                  <n-form-item label="Tipo Documento">
-                    <n-select :options="documentOptions" placeholder="" />
-                  </n-form-item>
-                </n-grid-item>
-                <n-grid-item>
-                  <n-form-item label="Documento">
-                    <n-input-number :show-button="false" placeholder="" />
-                  </n-form-item>
-                </n-grid-item>
-              </n-grid>
-            </n-grid-item>
-            <n-grid-item>
-              <n-form-item label="Nombre">
-                <n-input placeholder="" />
-              </n-form-item>
-            </n-grid-item>
-            <n-grid-item>
-              <n-form-item label="Dirección">
-                <n-input placeholder="" />
-              </n-form-item>
-            </n-grid-item>
-            <n-grid-item>
-              <n-form-item label="Correo">
-                <n-input placeholder="" />
-              </n-form-item>
-            </n-grid-item>
-            <n-grid-item>
-              <n-grid x-gap="12" :cols="2" >
-                <n-grid-item>
-                  <n-form-item label="Celular">
-                    <n-input-number :show-button="false" placeholder="" />
-                  </n-form-item>
-                </n-grid-item>
-                <n-grid-item>
-                  <n-form-item label="Fecha de Nacimiento">
-                    <n-date-picker type="date" placeholder="" clearable />
-                  </n-form-item>
-                </n-grid-item>
-              </n-grid>
-            </n-grid-item>
-            <n-grid-item>
-              <n-button type="primary">
-                Buscar
-              </n-button>
-            </n-grid-item>
+          <n-grid responsive="screen" cols="6 s:6 m:12 l:12 xl:24 2xl:24" :x-gap="12">
+            <n-form-item-gi label="Tipo Documento" :span="3">
+              <n-select :options="documentOptions" placeholder="" />
+            </n-form-item-gi>
+            <n-form-item-gi label="Documento" :span="3">
+              <n-input-number :show-button="false" placeholder="" />
+            </n-form-item-gi>
+            <n-form-item-gi label="Nombre" :span="6">
+              <n-input placeholder="" />
+            </n-form-item-gi>
+            <n-form-item-gi label="Dirección" :span="6">
+              <n-input placeholder="" />
+            </n-form-item-gi>
+            <n-form-item-gi label="Celular" :span="3">
+              <n-input-number :show-button="false" placeholder="" />
+            </n-form-item-gi>
+            <n-form-item-gi label="Fecha" :span="3">
+              <n-date-picker  type="date" placeholder="" clearable />
+            </n-form-item-gi>
+            <n-gi :span="6">
+              <n-button type="primary">Buscar</n-button>
+            </n-gi>
           </n-grid>
         </n-form>
       </n-collapse-item>
