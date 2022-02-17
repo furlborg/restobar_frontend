450963d226089a0955271fe76d2e6bdef52a0412
Jhonatan Hoyos Marina <jhoyosmarina@gmail.com>
2022-02-17 09:38:28 -0500

    Iniciando Reportes de mierda

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,24 @@
-import axios from "axios"
-import { useUserStore } from "@/store/modules/user"
-import { sleep } from "@/utils"
+import axios from "axios";
+import { useUserStore } from "@/store/modules/user";
+import { sleep } from "@/utils";
 
 export const http = axios.create({
-    baseURL: 'http://192.168.1.52:8000/api/v1/',
-})
+  baseURL: "http://192.168.1.52:8000/api/v1/",
+});
 
-http.interceptors.request.use(request => {
-    const userStore = useUserStore();
-    if (userStore.token) {
-        request.headers.common.Authorization = `Bearer ${userStore.token}`;
-    }
-    return request;
-})
+http.interceptors.request.use((request) => {
+  const userStore = useUserStore();
+  if (userStore.token) {
+    request.headers.common.Authorization = `Bearer ${userStore.token}`;
+  }
+  return request;
+});
+
+//*GAAAA
 
 http.interceptors.response.use(async (response) => {
-    if (process.env.NODE_ENV === 'development') {
-        await sleep()
-    }
-    return response
-})
+  if (process.env.NODE_ENV === "development") {
+    await sleep();
+  }
+  return response;
+});
