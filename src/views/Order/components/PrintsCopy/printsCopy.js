36d1770525f5462b6d99e2ad8aa781f1fba2ae74
Jhonatan Hoyos Marina <jhoyosmarina@gmail.com>
2022-04-04 17:59:47 -0500

    asdasd

--- src/views/Order/components/PrintsCopy/printsCopy.js
+++ src/views/Order/components/PrintsCopy/printsCopy.js
@@ -89,7 +89,7 @@ export const generatePrintCopy = (structure, height) => {
   });
 
   qz.websocket
-    .connect()
+    .connect({ host: "192.168.1.22" })
     .then(() => {
       return qz.printers.find();
     })
