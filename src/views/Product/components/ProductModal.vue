a3c354d315ec4cfeb649a44f3c5f8e12da5d4915
furlborg <oxvb4796@hotmail.com>
2022-04-26 09:35:08 -0500

    General fixes

--- src/views/Product/components/ProductModal.vue
+++ src/views/Product/components/ProductModal.vue
@@ -92,6 +92,7 @@
                     v-model:value="product.category"
                     :options="categoriesOptions"
                     placeholder=""
+                    filterable
                     clearable
                   />
                   <n-button
