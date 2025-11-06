000a38ae17acbe9741654a960d835983c3dd973b
Darkdredar <ncdredar@gmail.com>
2025-11-06 17:30:16 -0500

    refresh-categories-to-product-filter

--- src/views/Product/index.vue
+++ src/views/Product/index.vue
@@ -467,6 +467,7 @@ export default defineComponent({
     };
 
     onMounted(async () => {
+      await productStore.refreshCategories(); // 🔁 Actualiza categorías
       await loadProductsData();
     });
 
