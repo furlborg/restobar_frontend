f9d96ae274212c181370d41ee44eb748d9c39767
Jimmy Prada Guadalupe <jimmypradaguadalupe@Jimmys-MacBook-Pro.local>
2026-03-09 18:53:24 -0500

    fix delivery y para llevar boletas v2.2

--- src/views/Order/components/OrderTaking.vue
+++ src/views/Order/components/OrderTaking.vue
@@ -346,7 +346,7 @@ export default defineComponent({
       const updatedSale = { ...props.sale };
       updatedSale.customer = customer.id;
       updatedSale.customer_name = localCustomerName.value;
-      updatedSale.address = customer.addresses.length > 0 ? customer.addresses[0].id : null;
+      updatedSale.address = customer.addresses.length > 0 ? customer.addresses[0] : null;
       localDeliveryInfo.value.person = customer.names;
       localDeliveryInfo.value.phone = customer.phone;
       localDeliveryInfo.value.address = customer.addresses.length > 0 ? customer.addresses[0].description : null;
